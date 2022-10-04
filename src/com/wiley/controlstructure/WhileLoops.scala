4f47000d5b7c1be1534a01055dc76e1f50c6f637
marceloamor <kornexl9@gmail.com>
2022-10-04 16:34:27 +0100

    1st full day of Scala  commit, includes control structure, types, loops, mapping, basic list manipulation

---
+++ src/com/wiley/controlstructure/WhileLoops.scala
@@ -0,0 +1,10 @@
+package com.wiley.controlstructure
+
+object WhileLoops extends App {
+var a = 10
+  while(a<=20){
+    println(a)
+    a=a+2
+  }
+
+}
---
+++ src/com/wiley/variables/package.scala
@@ -0,0 +1,5 @@
+package com.wiley
+
+package object variables {
+
+}
